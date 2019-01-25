47d1a1348df59870a0c7ea1136945c4b5dcc1ff1
Kristjan Vilgo <Kristjan.VIlgo@elering.ee>
2019-01-26 01:45:31 +0200

    added type_view and made some optimizations

--- Tools/RDF_PARSER/RDF_parser.py
+++ Tools/RDF_PARSER/RDF_parser.py
@@ -19,7 +19,7 @@ import zipfile
 
 #pandas.set_option("display.height", 1000)
 pandas.set_option("display.max_rows", 30)
-pandas.set_option("display.max_columns", 4)
+pandas.set_option("display.max_columns", 8)
 pandas.set_option("display.width", 1000)
 
 # FUNCTIONS - go down for sample code
@@ -56,6 +56,11 @@ def load_RDF_to_dataframe(path_or_fileobject):
 
     data_list = []
 
+    #lets create all variables, so that in loops they are reused, rather than new ones are created, green thinking
+    ID      = ""
+    KEY     = ""
+    VALUE   = ""
+
 
     # TODO - a lot of replacemtns have been done using replace function, but is it valid that these charecaters are not present in UUID-s?
 
@@ -76,16 +81,25 @@ def load_RDF_to_dataframe(path_or_fileobject):
             VALUE = element.text
 
             if VALUE == None:
+
                 VALUE = element.attrib.values()[0].replace("urn:uuid:", "").replace("#_", "")
 
             #data_list.append([ID, ID_TYPE, KEY, VALUE]) # If using ID TYPE
             data_list.append([ID, KEY, VALUE, PROFILE_UUID])
 
 
+    end_time = datetime.datetime.now()
+    print("All values put to datalist", (end_time - start_time).total_seconds())
+
+    start_time = end_time
+
     data = pandas.DataFrame(data_list, columns = ["ID", "KEY", "VALUE", "PROFILE_UUID"])
 
     end_time = datetime.datetime.now()
-    print("Loaded to dataframe", (end_time - start_time).total_seconds())
+    print("Data list loaded to DataFrame", (end_time - start_time).total_seconds())
+
+
+
 
     return data
 
@@ -133,12 +147,24 @@ def load_all_to_dataframe(list_of_paths_to_zip_globalzip_xml):
 
     data = pandas.DataFrame()
 
-
     for xml in list_of_xmls:
+        print("Loading {}".format(xml))
         data = data.append(load_RDF_to_dataframe(xml), ignore_index = True)
 
     return data
 
+
+def type_view(data, type_name):
+
+    type_id_list = data.query("VALUE == '{}' & KEY == 'Type'".format(type_name))["ID"].tolist()
+
+    type_data = data[data.ID.isin(type_id_list)].drop_duplicates(["ID", "KEY"]) # There can't be duplicate ID and KEY pairs for pivot, but this will lose data on full model DependantOn and other info, solution would be to use pivot table function.
+
+    data_view = type_data.pivot(index="ID", columns = "KEY")["VALUE"]
+
+    return data_view
+
+pandas.DataFrame.type_view = type_view #Lets extend this fuctionality to pandas DataFrame
 
 # END OF FUNCTIONS
 
@@ -148,43 +174,84 @@ if __name__ == '__main__':
 
     path = "FlowExample.zip"
 
-
+    #path = r"C:\Users\kristjan.vilgo\Downloads\20180829T0130Z_NG_EQ_001.zip"
 
     data = load_all_to_dataframe([path])
-
-    print(data)
-
-
-    print("Loaded model UUID-s")
-    model_uuids = data[(data["VALUE"]=="FullModel")]
-    print(model_uuids)
-
-    print("Printing loaded profile headers")
-    for uuid in list(model_uuids["ID"]):
-
-        print(uuid)
-
-        header = data[(data["ID"]==uuid)]
-
-        print(header[["KEY", "VALUE"]])
-
 
     print("Loaded types")
     print(data[(data.KEY == "Type")]["VALUE"].value_counts())
 
-    print("Loaded attributes")
-    print(data.KEY.value_counts())
+    print(data.type_view("SubGeographicalRegion"))
+
+##    print(data)
+##
+##
+##    print("Loaded model UUID-s")
+##    model_uuids = data[(data["VALUE"]=="FullModel")]
+##    print(model_uuids)
+##
+##    print("Printing loaded profile headers")
+##    for uuid in list(model_uuids["ID"]):
+##
+##        print(uuid)
+##
+##        header = data[(data["ID"]==uuid)]
+##
+##        print(header[["KEY", "VALUE"]])
+##
+##
+##
+##    print("Loaded attributes")
+##    print(data.KEY.value_counts())
+##
+##
+##    print("All transformers rated S")
+##    print(data.query(KEY == "PowerTransformerEnd.ratedS"])
+##
+##
+##    print("Powertransformers")
+##    print(data[data.VALUE == "PowerTransformer"]) # Sample of not using query to sort/filter data
+##
+##    print("One winding data")
+##    print(data.query("ID == '12d773ab-1521-4e09-8a45-88b5eebf6fdd'")
 
 
-    print("All transformers rated S")
-    print(data[data.KEY == "PowerTransformerEnd.ratedS"])
+##    power_transformers = data.query("VALUE == 'PowerTransformer' & KEY == 'Type'")
+##
+##    terminals_conducting_equipment = data.query("KEY == 'Terminal.ConductingEquipment'")
+##
+##    terminals_svpowerflow = data.query("KEY == 'SvPowerFlow.Terminal'")
+##    terminals_svvoltage   = data.query("KEY == 'SvVoltage.TopologicalNode'")
+##
+##    eq_container_terminals = pandas.merge(power_transformers, terminals_conducting_equipment, how = "inner", left_on = 'ID', right_on = 'VALUE', suffixes = ["_eqcontainer", "_terminal"])
+##
+##    sv_powerflows = pandas.merge(eq_container_terminals, terminals_svpowerflow, how = "inner", left_on = "ID_terminal", right_on = "VALUE", suffixes = ["", "_svpowerflow"])
 
 
-    print("Powertransformers")
-    print(data[data.VALUE == "PowerTransformer"])
+##KEY                                  IdentifiedObject.name          SubGeographicalRegion.Region                   Type
+##ID
+##02e3f63a-1100-48fa-a64b-79b3b86d28e3                  Grid  2fdc1414-2e27-46c9-9989-860d4f8d420b  SubGeographicalRegion
+##2b35c4e0-d85f-44b7-9e2c-4a110f9944a3      SubstatSeriesImp  2fdc1414-2e27-46c9-9989-860d4f8d420b  SubGeographicalRegion
+##42a937f4-fd79-4d57-8153-14d40107b23a           SubstatMain  2fdc1414-2e27-46c9-9989-860d4f8d420b  SubGeographicalRegion
+##43fd1008-34f6-4721-a468-64339d340cff        SubstatTr2tap5  2fdc1414-2e27-46c9-9989-860d4f8d420b  SubGeographicalRegion
+##95d267b2-3ee1-4a9d-a400-36bfb8032412            SubstatTr2  2fdc1414-2e27-46c9-9989-860d4f8d420b  SubGeographicalRegion
+##a598110a-4286-44e1-8335-56c866363cd7           SubstatLine  2fdc1414-2e27-46c9-9989-860d4f8d420b  SubGeographicalRegion
+##c9ce2b09-1225-4afa-bf85-9284fdb1f415           SubstatTr3a  2fdc1414-2e27-46c9-9989-860d4f8d420b  SubGeographicalRegion
+##eba981b5-0007-40bb-8f5c-f14c277f5d61           SubstatTr3b  2fdc1414-2e27-46c9-9989-860d4f8d420b  SubGeographicalRegion
+##f4637966-e63c-4bb4-89ed-0f897e40b05c          SubstatEqLne  2fdc1414-2e27-46c9-9989-860d4f8d420b  SubGeographicalRegion
 
-    print("One winding data")
-    print(data[data.ID == "12d773ab-1521-4e09-8a45-88b5eebf6fdd"])
+
+
+
+
+
+
+
+
+
+
+
+
 
     # for quick export of data use data[data.VALUE == "PowerTransformer"].to_csv(export.csv) or data[data.VALUE == "PowerTransformer"].to_clipboard() and  paste to excel, for other method refer to pandas manual
 
