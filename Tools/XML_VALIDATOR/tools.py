837250a8bd75b25bad7e3d605c9ce20fce573285
kristjan <vilgo44@hotmail.com>
2019-02-20 20:06:03 +0200

    new version of validator + GUI

---
+++ Tools/XML_VALIDATOR/tools.py
@@ -0,0 +1,95 @@
+import os
+from lxml import etree
+import pandas
+
+
+def check_path(list_of_paths):  # Print paths and check if they exist
+
+    for path in list_of_paths:
+        check = os.path.exists(path)
+        print("{} > path: {}".format(path, check))
+
+
+def xml_path_to_str(xml_path_or_fileobject):
+    """"path to xml file -> pretty xml as string"""
+
+    xml_str = etree.tostring(etree.parse(xml_path_or_fileobject).getroot(), pretty_print = True).decode()
+    return xml_str
+
+
+def list_of_files(root_path,file_extension, go_deep = False):
+
+    path_list = [root_path]
+
+    matches = []
+
+
+    for path in path_list:
+
+        if os.path.isdir(path) == True:
+            print("{} is not a path".format(path))
+
+            for filename in os.listdir(path):
+
+                full_path = os.path.join(path, filename)
+
+                if filename.endswith(file_extension):
+                    matches.append(full_path)
+
+                else:
+                    print ("Not a {} file: {}".format(file_extension, filename))
+
+                    if go_deep == True:
+                        print("Adding path to futher processing -> {}".format(full_path))
+                        path_list.append(full_path)
+
+
+    return matches
+
+
+def find_all_xsds():
+
+    file_list = list_of_files("XSD",".xsd", go_deep=True)
+
+    xsd_list = []
+
+    for file_path in file_list:
+
+
+        single_xsd_meta = {}
+
+        tree_object = etree.parse(file_path)
+
+        single_xsd_meta["target_namespace"] = tree_object.getroot().attrib.get("targetNamespace", "")
+        single_xsd_meta["file_path"]        = os.path.abspath(file_path)
+        single_xsd_meta["file_name"]        = os.path.basename(file_path)
+
+        xsd_list.append(single_xsd_meta)
+
+    xsd_dataframe = pandas.DataFrame(xsd_list)
+
+    return xsd_dataframe
+
+
+def load_xml_string(XML_string):
+
+    error = ""
+
+    # Load XML
+    print("Loading XML xml string")
+    try:
+        parser = etree.XMLParser(remove_comments=True, encoding='utf-8')
+        xml_doc = etree.fromstring(XML_string.encode(), parser=parser)
+        print("XML loaded")
+
+    except etree.XMLSyntaxError as error:
+
+        print("Loading XML failed")
+        #print(error)
+        print(error.args)
+
+        return error
+
+
+    return xml_doc
+
---
+++ Tools/XML_VALIDATOR/xsd_gui.py
@@ -0,0 +1,85 @@
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+import ace_components
+from dash.dependencies import Input, Output, State
+
+from lxml import etree
+
+import xsd
+
+from tools import *
+
+
+app = dash.Dash()
+
+external_css = ["https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css",
+                "https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css", ]
+
+for css in external_css:
+    app.css.append_css({"external_url": css})
+
+external_js = [ "https://code.jquery.com/jquery-3.3.1.min.js",
+                "https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js",
+                "https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"]
+
+for js in external_js:
+    app.scripts.append_script({"external_url": js})
+
+
+app.layout = html.Div(children=[
+
+    html.Div(children = [
+    ace_components.Ace(
+
+        id='xml',
+        focus=True,
+        mode='xml',
+        theme='monokai',
+        className = "col-sm",
+        cursorStart = 1,
+        style= {"width":"100%", "height":"80vh"},
+        fontSize = 14,
+        value= xml_path_to_str("30345325.xml"),
+        showGutter = True,
+        highlightActiveLine = True,
+
+    ),
+
+    ace_components.Ace(
+        id='error_log',
+        focus=False,
+        mode='Text',
+        theme='monokai',
+        className = "col-sm",
+        style= {"width":"100%", "height":"20vh"},
+        fontSize = 14,
+        value= "",
+
+    )
+    ])
+],className="container-fluid")
+
+@app.callback(Output('error_log', 'value'),
+              [Input('xml', 'value')])
+def display_output(content): # TODO higlight erronous rows in xml editor
+
+
+    errors_list = xsd.validate_XML_string(content)
+    errors_string = ""
+
+    if type(errors_list) == list:
+
+        for error in errors_list:
+            errors_string += "line {} - {}\n".format(error["line"], error["message"])
+
+    else:
+        errors_string = str(errors_list)
+
+    return errors_string
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
+
+
+
