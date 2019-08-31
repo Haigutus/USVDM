e6b68ee3dfc5c839fe36108812d365149d63f03a
Kristjan Vilgo <Kristjan.VIlgo@elering.ee>
2019-08-31 03:43:30 +0300

    RDF parser enhanced
    1. Prefix replace now works only on first characters
    2. type_view converts to numbers what it can
    3. file extension can be both with small and big letters
    4. parsing was made faster and redundat code removed

--- Tools/RDF_PARSER/RDF_parser.py
+++ Tools/RDF_PARSER/RDF_parser.py
@@ -18,7 +18,9 @@ import datetime
 import zipfile
 import uuid
 
-#from multiprocessing import Pool - TODO add parallel loading for import ALL
+#from collections import deque
+
+#from multiprocessing import Pool - TODO add parallel loading for import ALL DASK, SPARK, MODIN, VAEX
 
 
 #pandas.set_option("display.height", 1000)
@@ -41,44 +43,114 @@ def print_duration(text, start_time):
 
     return duration, end_time
 
+def remove_prefix(original_string, prefix_string):
+    "Removes prefix from a string"
+
+    prefix_lenght = len(prefix_string)
+
+    if original_string[0:prefix_lenght] == prefix_string:
+        return original_string[prefix_lenght:]
+
+    return original_string
+
 def clean_ID(ID):
+    """Removes ID prefixes used in CIM - first occourance of 'urn:uuid:', '#_', '_' is replaced by empty string"""
 
     # TODO - a lot of replacements have been done using replace function, but is it valid that these charecaters are not present in UUID-s? is replace once sufficent?
 
-    replace_count = 1 # Remove only once the ID prefix string, otherwise we risk of removing characters from within ID
-    clean_ID      = ID.replace("urn:uuid:", "", replace_count).replace("#_", "", replace_count).replace("_", "", replace_count)
+    #replace_count = 1 # Remove only once the ID prefix string, otherwise we risk of removing characters from within ID
+    #clean_ID      = ID.replace("urn:uuid:", "", replace_count).replace("#_", "", replace_count).replace("_", "", replace_count)
+    ID = remove_prefix(ID, "urn:uuid:")
+    ID = remove_prefix(ID, "#_")
+    ID = remove_prefix(ID, "_")
 
-    return clean_ID
+    return ID
 
 
-def load_RDF_objects_from_XML(path_or_fileobject):
+def load_RDF_objects_from_XML(path_or_fileobject, debug = False):
 
     # START TIMER
-
-    start_time = datetime.datetime.now()
+    if debug:
+        start_time = datetime.datetime.now()
 
     # LOAD XML
-
-    parser     = etree.XMLParser(remove_comments=True, collect_ids=False)   # TODO - try remove_blank_text, remove_pis
+    parser     = etree.XMLParser(remove_comments=True, collect_ids=False, remove_blank_text=True)
     parsed_xml = etree.parse(path_or_fileobject, parser = parser)           # TODO - add iterparse for Python3
 
     # Get unique ID for loaded instance
-
     instance_id = clean_ID(parsed_xml.find("./").attrib.values()[0]) # Lets asume that the first RDF element describes the whole document - TODO replace it with hash of whole XML
-    #model_id = parsed_xml.find(".//{http://iec.ch/TC57/61970-552/ModelDescription/1#}FullModel").attrib.values()[0].replace("urn:uuid:", "")
-    #_,start_time = print_duration("XML loaded to tree object", start_time)
+
+    if debug:
+        _, start_time = print_duration("XML loaded to tree object", start_time)
 
 
     # EXTRACT RDF OBJECTS
     RDF_objects = parsed_xml.getroot().iterchildren()
-    #_,start_time = print_duration("All children put to a generator", start_time)
+
+    if debug:
+        _, start_time = print_duration("All children put to a generator", start_time)
 
 
 
     return RDF_objects, instance_id
 
 
-def load_RDF_to_dataframe(path_or_fileobject):
+def find_all_xmls(list_of_paths_to_zip_globalzip_xml, debug = False):
+    """Retunrs list of file objects and/or paths"""
+
+    xml_files_list = []
+    zip_files_list = [] # TODO - add support random folders awell
+
+    for item in list_of_paths_to_zip_globalzip_xml:
+
+
+        item_lower = item.lower()
+
+        if ".xml" in item_lower or ".rdf" in item_lower: # TODO - add item.lower()
+            xml_files_list.append(item)
+
+            if debug:
+                print("Added: {}".format(item))
+
+        elif ".zip" in item_lower:
+            zip_files_list.append(item)
+
+            if debug:
+                print("Added for further processing: {}".format(item))
+
+        else: print("WARNING 1 - Not supported file: {}".format(item))
+
+
+    for zip_file_path in zip_files_list:
+
+        zip_container = zipfile.ZipFile(zip_file_path)
+        zipped_files  = zip_container.namelist()
+
+        for zipped_file in zipped_files:
+
+            zipped_file_lower = zipped_file.lower()
+
+            if ".xml" in zipped_file_lower or ".rdf" in zipped_file_lower:
+                file_object = BytesIO(zip_container.read(zipped_file))
+                file_object.name = zipped_file
+                xml_files_list.append(file_object)
+
+                if debug:
+                    print("Added: {}".format(zipped_file))
+
+            elif ".zip" in zipped_file_lower:
+                zip_files_list.append(BytesIO(zip_container.read(zipped_file)))
+
+                if debug:
+                    print("Added for further processing: {}".format(zipped_file))
+
+            else: print("WARNING 2 - Not supported file: {}".format(zipped_file))
+
+
+    return xml_files_list
+
+
+def load_RDF_to_list(path_or_fileobject, debug = False):
 
     file_name = path_or_fileobject
 
@@ -87,70 +159,11 @@ def load_RDF_to_dataframe(path_or_fileobject):
 
     print("Loading {}".format(file_name))
 
+    RDF_objects, INSTANCE_ID = load_RDF_objects_from_XML(path_or_fileobject, debug)
 
-    RDF_objects, INSTANCE_ID = load_RDF_objects_from_XML(path_or_fileobject)
+    if debug:
+        start_time = datetime.datetime.now()
 
-    start_time = datetime.datetime.now()
-
-    data_list = [(str(uuid.uuid4()), "Lable", file_name, INSTANCE_ID)] # Lets generate list object for all of the RDF data and store the original filename under rdf:Lable
-
-    #lets create all variables, so that in loops they are reused, rather than new ones are created, green thinking
-    ID      = ""
-    KEY     = ""
-    VALUE   = ""
-
-
-    # TODO - a lot of replacements have been done using replace function, but is it valid that these charecaters are not present in UUID-s?
-
-    for object in RDF_objects:
-
-        replace_count = 1 # Remove only once the ID prefix string, otherwise we risk of removing characters from within ID
-
-        ID = clean_ID(object.attrib.values()[0])
-        #ID = object.attrib.values()[0].replace("urn:uuid:", "").replace("#_", "").replace("_", "")
-        #ID_TYPE = object.attrib.keys()[0].split("}")[1] # Adds column to identifi "ID" and "about" types of ID
-        #KEY = '{http://www.w3.org/1999/02/22-rdf-syntax-ns#}Type' # If we would like to keep all with correct namespace
-        KEY = 'Type' # But we don't
-        VALUE = object.tag.split("}")[1]
-
-        #data_list.append([ID, ID_TYPE, KEY, VALUE]) # If using ID TYPE
-        data_list.append([ID, KEY, VALUE, INSTANCE_ID])
-
-        for element in object.iterchildren():
-
-            KEY = element.tag.split("}")[1]
-            VALUE = element.text
-
-            if VALUE == None and len(element.attrib.values()) > 0:
-
-                VALUE = clean_ID(element.attrib.values()[0])
-
-
-
-            #data_list.append([ID, ID_TYPE, KEY, VALUE]) # If using ID TYPE
-            data_list.append((ID, KEY, VALUE, INSTANCE_ID))
-
-    #_,start_time = print_duration("All values put to data list", start_time)
-
-
-    data = pandas.DataFrame(data_list, columns = ["ID", "KEY", "VALUE", "INSTANCE_ID"])
-    #_,start_time = print_duration("Data list loaded to DataFrame", start_time)
-
-    return data
-
-
-def load_RDF_to_list(path_or_fileobject):
-
-    file_name = path_or_fileobject
-
-    if type(path_or_fileobject) != str:
-        file_name = path_or_fileobject.name
-
-    print("Loading {}".format(file_name))
-
-    RDF_objects, INSTANCE_ID = load_RDF_objects_from_XML(path_or_fileobject)
-
-    start_time = datetime.datetime.now()
 
     data_list = [(str(uuid.uuid4()), "Lable", file_name, INSTANCE_ID)] # Lets generate list object for all of the RDF data and store the original filename under rdf:Lable
 
@@ -161,15 +174,13 @@ def load_RDF_to_list(path_or_fileobject):
 
     for object in RDF_objects:
 
-        ID      = clean_ID(object.attrib.values()[0])
-        #ID = object.attrib.values()[0].replace("urn:uuid:", "").replace("#_", "").replace("_", "")
-        #ID = ID_regex.sub("",object.attrib.values()[0]) # Was slower than regular replace, but maybe regex can be optimized
-        #ID_TYPE = object.attrib.keys()[0].split("}")[1] # Adds column to identifi "ID" and "about" types of ID
-        #KEY = '{http://www.w3.org/1999/02/22-rdf-syntax-ns#}Type' # If we would like to keep all with correct namespace
-        KEY     = 'Type'
-        VALUE   = object.tag.split("}")[1]
+        ID          = clean_ID(object.attrib.values()[0])
+        #KEY        = '{http://www.w3.org/1999/02/22-rdf-syntax-ns#}Type' # If we would like to keep all with correct namespace
+        KEY         = 'Type'
+        VALUE       = object.tag.split("}")[1]
+        #ID_TYPE    = object.attrib.keys()[0].split("}")[1] # Adds column to identifi "ID" and "about" types of ID
 
-        #data_list.append([ID, ID_TYPE, KEY, VALUE]) # If using ID TYPE
+        #data_list.append([ID, ID_TYPE, KEY, VALUE]) # If using ID TYPE, maybe also namespace should be kept?
         data_list.append((ID, KEY, VALUE, INSTANCE_ID))
 
         for element in object.iterchildren():
@@ -181,62 +192,40 @@ def load_RDF_to_list(path_or_fileobject):
 
                 VALUE = clean_ID(element.attrib.values()[0])
 
-            #data_list.append([ID, ID_TYPE, KEY, VALUE]) # If using ID TYPE
+            #data_list.append([ID, ID_TYPE, KEY_NAMESPACE, KEY, VALUE]) # If using ID TYPE
             data_list.append((ID, KEY, VALUE, INSTANCE_ID))
 
-    #_,start_time = print_duration("All values put to data list", start_time)
-
+    if debug:
+        _,start_time = print_duration("All values put to data list", start_time)
 
     return data_list
 
-def find_all_xmls(list_of_paths_to_zip_globalzip_xml):
-    """Retunrs list of file objects and/or paths"""
-
-    xml_files_list = []
-    zip_files_list = [] # TODO - add support random folders awell
-
-    for item in list_of_paths_to_zip_globalzip_xml:
 
 
-        if ".xml" in item or ".rdf" in item or ".XML" in item: # TODO - add item.lower()
-            xml_files_list.append(item)
-            #print("Added: {}".format(item))
+def load_RDF_to_dataframe(path_or_fileobject, debug = False):
+    """Parse single file to Pandas DataFrame"""
 
-        elif ".zip" in item:
-            zip_files_list.append(item)
-            #print("Added for further processing: {}".format(item))
-
-        else: print("Not supported file: {}".format(item))
+    data_list = load_RDF_to_list(path_or_fileobject, debug)
 
 
-    for zip_file_path in zip_files_list:
+    if debug:
+        start_time = datetime.datetime.now()
 
-        zip_container = zipfile.ZipFile(zip_file_path)
-        zipped_files  = zip_container.namelist()
+    data = pandas.DataFrame(data_list, columns = ["ID", "KEY", "VALUE", "INSTANCE_ID"])
 
-        for zipped_file in zipped_files:
+    if debug:
+        _, start_time = print_duration("List of data loaded to DataFrame", start_time)
 
-            if ".xml" in zipped_file or ".rdf" in zipped_file or ".XML" in zipped_file:
-                file_object = BytesIO(zip_container.read(zipped_file))
-                file_object.name = zipped_file
-                xml_files_list.append(file_object)
-                #print("Added: {}".format(zipped_file))
+    return data
 
-            elif ".zip" in zipped_file:
-                zip_files_list.append(BytesIO(zip_container.read(zipped_file)))
-                #print("Added for further processing: {}".format(zipped_file))
+def load_all_to_dataframe(list_of_paths_to_zip_globalzip_xml, debug = False):
+    """Parse contents of provided list of paths to Pandas DataFrame (zip, global zip or XML)"""
 
-            else: print("Not supported file: {}".format(zipped_file))
+    if debug:
+        process_start = datetime.datetime.now()
 
+    list_of_xmls = find_all_xmls(list_of_paths_to_zip_globalzip_xml, debug)
 
-    return xml_files_list
-
-def load_all_to_dataframe(list_of_paths_to_zip_globalzip_xml):
-    "Parse contents of provided list of paths to Pandas DataFrame (zip, global zip or XML)"
-
-    list_of_xmls = find_all_xmls(list_of_paths_to_zip_globalzip_xml)
-
-    #data = pandas.DataFrame()
     data_list = []
 
 ##    TODO - add paralel processing if number inputs is greater than X - top be decided
@@ -245,13 +234,17 @@ def load_all_to_dataframe(list_of_paths_to_zip_globalzip_xml):
 
     for xml in list_of_xmls:
 
-        data_list.extend(load_RDF_to_list(xml))
+        data_list.extend(load_RDF_to_list(xml, debug))
 
+    if debug:
+        start_time = datetime.datetime.now()
 
-    start_time = datetime.datetime.now()
     data = pandas.DataFrame(data_list, columns = ["ID", "KEY", "VALUE", "INSTANCE_ID"])
 
-    #_,start_time = print_duration("Data list loaded to DataFrame", start_time)
+    if debug:
+        print_duration("Data list loaded to DataFrame", start_time)
+        print_duration("All loaded in ", process_start)
+        #print(data.info())
 
     return data
 
@@ -267,6 +260,7 @@ def type_tableview(data, type_name):
     type_data = pandas.merge(type_id[["ID"]], data, right_on = "ID", left_on = "ID").drop_duplicates(["ID", "KEY"]) # There can't be duplicate ID and KEY pairs for pivot, but this will lose data on full model DependantOn and other info, solution would be to use pivot table function.
 
     data_view = type_data.pivot(index="ID", columns = "KEY")["VALUE"]
+    data_view = data_view.apply(pandas.to_numeric, errors='ignore') # Convert to numbers all columns that contain only numbers
 
     return data_view
 
@@ -311,10 +305,10 @@ if __name__ == '__main__':
 
     #path = r"C:\Users\kristjan.vilgo\Downloads\20190216T2130Z_1D_LITGRID_TP_001.zip"
 
-    data = load_all_to_dataframe([path])
+    data = load_all_to_dataframe([path], debug = True)
 
-    print("Loaded types")
-    print(data[(data.KEY == "Type")]["VALUE"].value_counts())
+    #print("Loaded types")
+    #print(data[(data.KEY == "Type")]["VALUE"].value_counts())
 
     #print(data.type_tableview("ACLineSegment"))
 
