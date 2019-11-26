b70fba40e602a88e4e4b0de567fdc86aab72b5e8
Kristjan Vilgo <Kristjan.VIlgo@elering.ee>
2019-11-26 19:42:49 +0200

    QAS api update

--- Tools/MADES_API/QAS_API.py
+++ Tools/MADES_API/QAS_API.py
@@ -17,6 +17,8 @@ from datetime import datetime
 import zipfile
 import StringIO
 
+import time
+
 
 
 from collections import OrderedDict
@@ -56,7 +58,6 @@ def IGM_query_message(processType, tso, scenarioDate, scenarioTime="", version="
 
     """ <xs:element name="processType" type="entso-e:ProcessType"/>
         <xs:element name="tso" type="entso-e:tso"/>
-        <xs:element name="CGMType" type="entso-e:CGMType"/>
         <xs:element name="scenarioDate" type="xs:date"/>
         <xs:element name="scenarioTime" type="xs:time" minOccurs="0" maxOccurs="1"/>
         <xs:element name="version" type="xs:int" minOccurs="0" maxOccurs="1">"""
@@ -64,6 +65,7 @@ def IGM_query_message(processType, tso, scenarioDate, scenarioTime="", version="
 
     # TODO - validate fileds
 
+    # Put metadata to ordered dict to keep correct sequence for XML generation
     meta_dict = OrderedDict()
     meta_dict["processType"]     = processType
     meta_dict["tso"]             = tso
@@ -85,6 +87,7 @@ def CGM_query_message(processType, RSCName, CGMType, scenarioDate, scenarioTime=
 
     # TODO - validate fileds
 
+    # Put metadata to ordered dict to keep correct sequence for XML generation
     meta_dict = OrderedDict()
     meta_dict["processType"]     = processType
     meta_dict["RSCName"]         = RSCName
@@ -152,19 +155,18 @@ def parse_QAReport(xml_string):
 
 
 
+# Enumerations defined for messages
 
-tsos=['50Hertz', 'D8', 'Amprion', 'D7', 'APG', 'AT', 'CEPS', 'CZ', 'CGES',\
- 'ME', 'ELES', 'SI', 'ELIA', 'BE', 'EMS', 'RS', 'DKW', 'D1', 'ESO', 'BG', \
- 'HOPS', 'HR', 'IPTO', 'GR', 'MAVIR', 'HU', 'MEPSO', 'MK', 'NOSBiH', 'BA',\
-  'OST', 'AL', 'PSE', 'PL', 'REE', 'ES', 'REN', 'PT', 'RTE', 'FR', 'SEPS',\
-   'SK', 'Swissgrid', 'CH', 'TEIAS', 'TR', 'TTN', 'NL', 'D2', 'TTG', 'Terna',\
-    'IT', 'Transelectrica', 'RO', 'TransnetBW', 'D4', 'Creos', 'LU', 'DKE',\
-     'DK', 'Fingrid', 'FI', 'Statnett', 'NO', 'SE', 'AST', 'LV', 'Elering',\
-      'EE', 'Litgrid', 'LT', 'GB', 'NationalGrid', 'NG', 'NGET', 'Eirgrid',\
-       'EIRGRIDSONI', 'Ukrenergo', 'WEPS']
+tso_list = ['50Hertz', 'Amprion', 'APG', 'CEPS', 'CGES', 'ELES', 'ELIA', 'EMS',
+'Energinet.DK West', 'ESO', 'HOPS', 'IPTO', 'MAVIR', 'MEPSO', 'NOSBiH', 'OST',
+'PSE', 'REE', 'REN', 'RTE', 'SEPS', 'Swissgrid', 'TEIAS', 'TenneT Neth.',
+'TenneT. Ger', 'Terna', 'Transelectrica', 'TransnetBW', 'Creos',
+'Energinet.DK East', 'Fingrid', 'Statnett', 'SE', # 'Svenska Kraftn\xc3\xa4t' python 27 does not support well non ascii
+'AST', 'Elering', 'Litgrid', 'National Grid', 'Eirgrid/SONI', 'UKRENERGO']
 
-areas = ['ba', 'ce', 'eu', 'in', 'no', 'uk']
-RSCs  = ['balticrsc', 'coreso', 'nordicrsc', 'sccrsci', 'tscnet']
+RSCname_list  = ['balticrsc', 'coreso', 'nordicrsc', 'sccrsci', 'tscnet']
+
+CGMType_list  = ['ba', 'ce', 'eu', 'in', 'no', 'uk']
 
 
 def parse_error_message(message):
@@ -192,65 +194,62 @@ service = create_client(server, username, password, debug = False)
 
 
 TSOs = ["AST", "ELERING", "Litgrid"]
+TSOs = tso_list
 
 query_counter = 0
 
-##for TSO in TSOs:
-##    query_message = IGM_query_message(processType="1D", tso=TSO, scenarioDate="2019-09-01")
-##    message_ID = service.send_message("SERVICE-QAS", "ENTSOE-QAS-Query", query_message)
-##    print(message_ID)
+for TSO in TSOs:
+    query_message = IGM_query_message(processType="1D", tso=TSO, scenarioDate="2019-09-01", scenarioTime="09:30:00")
+    message_ID = service.send_message("SERVICE-QAS", "ENTSOE-QAS-Query", query_message)
+    print(message_ID)
 
     ##status = service.check_message_status(message_ID)
 
-##    query_counter += 1
+    query_counter += 1
 
 
-#while query_counter > 0:
+while query_counter > 0:
 
-message = service.receive_message("ENTSOE-QAS-QueryResult")
+    message = service.receive_message("ENTSOE-QAS-QueryResult")
 
+    error = False
+    try:
+        parse_error_message(message)
+        error = True
 
+        print("Removing error message")
+        service.confirm_received_message(message['receivedMessage']['messageID'])
+        print(message['receivedMessage']['messageID'])
 
-
-
-
-
-
-
-
-
-
-error = False
-try:
-    parse_error_message(message)
-    error = True
-
-    print("Removing error message")
-    message = service.receive_message("ENTSOE-QAS-QueryResult", False)
-    service.confirm_received_message(message['receivedMessage']['messageID'])
-    print(message['receivedMessage']['messageID'])
-
-    query_counter -= 1
-
-except:
-    pass
+        query_counter -= 1
 
 
-if error == False:
-    zip_file = parse_zip_attachment(message)
-    #print(zip_file.namelist())
 
-    raw_files = [zip_file.read(name) for name in zip_file.namelist()]
+    except:
+        pass
 
-    meta_list = []
-    for xml_string in raw_files:
 
-        meta_list.append(parse_QAReport(xml_string))
+    if error == False and type(message) != dict:
+        zip_file = parse_zip_attachment(message)
+        print(zip_file.namelist())
 
-    print(pandas.DataFrame(meta_list).drop(columns=["violations", "ERRORS_INFO", "WARNINGS_INFO"]))
+        raw_files = [zip_file.read(name) for name in zip_file.namelist()]
+
+        meta_list = []
+        for xml_string in raw_files:
+
+            meta_list.append(parse_QAReport(xml_string))
+
+        print(pandas.DataFrame(meta_list).drop(columns=["violations", "ERRORS_INFO", "WARNINGS_INFO"]))
 
     #pandas.DataFrame(meta_list)#.drop(columns=["violations"]).to_csv("C:\Users\kristjan.vilgo\Downloads\example_data.csv")
 
+    message = service.receive_message("ENTSOE-QAS-QueryResult", False)
+    service.confirm_received_message(message['receivedMessage']['messageID'])
+
+
+    time.sleep(1)
+    query_counter -= 1
 
 ##message = service.receive_message("ENTSOE-QAS-QueryResult", False)
 ##service.confirm_received_message(message['receivedMessage']['messageID'])
@@ -273,16 +272,16 @@ if error == False:
 ##</q:QASQuery>
 ##"""
 ##
-##IGM_example_query = """
-##<q:QASQuery created="2008-09-29T03:49:45" schemeVersion="1" xmlns:q="http://entsoe.eu/checks">
-##  <q:IGM>
-##    <q:processType>1D</q:processType>
-##    <q:tso>RTE</q:tso>
-##    <q:scenarioDate>2019-06-25</q:scenarioDate>
-##    <!--Optional:-->
-##    <q:scenarioTime>00:30:00</q:scenarioTime> <!-- if defined, only one timestamp will be returned -->
-##    <!--<q:version>3</q:version> -->                  <!-- if not defined, latest version will be returned -->
-##  </q:IGM>
-##</q:QASQuery>
-##"""
+IGM_example_query = """
+<q:QASQuery created="2008-09-29T03:49:45" schemeVersion="1" xmlns:q="http://entsoe.eu/checks">
+  <q:IGM>
+    <q:processType>1D</q:processType>
+    <q:tso>ELERING</q:tso>
+    <q:scenarioDate>2019-06-25</q:scenarioDate>
+    <!--Optional:-->
+    <q:scenarioTime>00:30:00</q:scenarioTime> <!-- if defined, only one timestamp will be returned -->
+    <!--<q:version>3</q:version> -->          <!-- if not defined, latest version will be returned -->
+  </q:IGM>
+</q:QASQuery>
+"""
 
