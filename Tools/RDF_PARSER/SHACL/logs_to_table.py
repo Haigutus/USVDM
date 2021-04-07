73a51d99f0ef5a559c6ecc22367d8a97ab112ff6
kristjan.vilgo <kristjan.vilgo@gmail.com>
2021-04-07 03:36:24 +0300

    Added duration statistaics

--- Tools/RDF_PARSER/SHACL/logs_to_table.py
+++ Tools/RDF_PARSER/SHACL/logs_to_table.py
@@ -2,6 +2,7 @@ import pandas
 import json
 import glob
 import datetime
+import aniso8601
 
 path_to_log_files = r"C:\Users\kristjan.vilgo\Downloads\OPDM_VALIDATOR_LOGS"
 
@@ -13,5 +14,27 @@ parsed_data_list = []
 for log_file in input_files:
     parsed_data_list.append(pandas.json_normalize(json.load(open(log_file, "r"))))
 
-# Merge results and save as excel
-pandas.concat(parsed_data_list, ignore_index=True).to_excel(f"logs_in_table_{datetime.datetime.now():%Y-%m-%dT%H%M%S}.xlsx")
+# Merge results
+data = pandas.concat(parsed_data_list, ignore_index=True)
+
+# Get duration statistics
+duration_statistics = data.dropna(subset=["source.proc-alive"]).copy()
+duration_statistics["source.proc-alive"] = duration_statistics["source.proc-alive"].apply(aniso8601.parse_duration)
+durations_pivot = pandas.pivot_table(duration_statistics, index=['source.source'], columns=['source.log-command'], aggfunc="sum", values=["source.proc-alive"])
+print(durations_pivot)
+
+# Define reports and names
+file_name = f"logs_in_table_{datetime.datetime.now():%Y-%m-%dT%H%M%S}.xlsx"
+reports = {"log_table": data,
+           "duration_statistics": durations_pivot}
+
+# Export reports to excel
+with pandas.ExcelWriter(file_name, datetime_format='hh:mm:ss.000') as writer:
+
+    # Raw reports
+    for report_name, report in reports.items():
+        pandas.DataFrame(report).to_excel(writer, sheet_name=report_name)
+
+print(f"INFO - created file: {file_name}")
+
+#data[['source.timestamp', 'source.log-message', 'source.source', 'source.log-command', 'source.proc-alive']]
