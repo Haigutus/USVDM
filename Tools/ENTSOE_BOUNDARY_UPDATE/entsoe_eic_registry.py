592ac72f76ab7328ef705896b4f8c719927b87bb
kristjan.vilgo <kristjan.vilgo@gmail.com>
2021-11-04 00:59:56 +0200

    Added EIC registry parsing to triplets

--- Tools/ENTSOE_BOUNDARY_UPDATE/entsoe_eic_registry.py
+++ Tools/ENTSOE_BOUNDARY_UPDATE/entsoe_eic_registry.py
@@ -2,6 +2,9 @@ import requests
 import pandas
 from lxml import etree
 
+pandas.set_option('display.max_columns', 20)
+pandas.set_option('display.width', 1000)
+
 
 def get_allocated_eic():
 
@@ -12,12 +15,61 @@ def get_allocated_eic():
 
     eic_data_list = []
 
-    for EIC in xml_tree.iter("{*}EICCode_MarketDocument"):
-        eic_dict = {}
+    EICs = xml_tree.iter("{*}EICCode_MarketDocument")
 
-        for field in EIC.getchildren():
-            eic_dict[field.tag.split("}")[1]] = field.text
+    for EIC in EICs:
 
-        eic_data_list.append(eic_dict)
+        elements = [EIC]
+        for element in elements:
+
+            eic_dict = {}
+
+            for field in element.getchildren():
+                if len(field.getchildren()) == 0:
+                    eic_dict[field.tag.split("}")[1]] = field.text
+                else:
+                    elements.append(field)
+
+            eic_data_list.append(eic_dict)
 
     return pandas.DataFrame(eic_data_list)
+
+
+def eic_table_to_triplet(data):
+    return data.melt(id_vars="mRID", value_name="VALUE", var_name="KEY").rename(columns={"mRID": "ID"})
+
+def get_allocated_eic_triplet():
+    allocated_eic_url = "https://www.entsoe.eu/fileadmin/user_upload/edi/library/eic/allocated-eic-codes.xml"
+    allocated_eic = requests.get(allocated_eic_url)
+
+    xml_tree = etree.fromstring(allocated_eic.content)
+
+    eic_data_list = []
+
+    EICs = xml_tree.iter("{*}EICCode_MarketDocument")
+
+    for EIC in EICs:
+        ID = EIC[0].text
+
+        elements = [{"element": EIC}]
+        for element in elements:
+
+            for field in element["element"].getchildren():
+
+                parent_name = element.get("parent_name")
+                element_name = element['element'].tag.split('}')[1]
+                field_name = field.tag.split('}')[1]
+
+                if not parent_name:
+                    parent_name = element_name
+                else:
+                    parent_name = f"{parent_name}.{element_name}"
+
+                name = f"{parent_name}.{field_name}"
+
+                if len(field.getchildren()) == 0:
+                    eic_data_list.append({"ID": ID, "KEY": name, "VALUE": field.text})
+                else:
+                    elements.append({"parent_name": parent_name, "element": field})
+
+    return(pandas.DataFrame(eic_data_list))
