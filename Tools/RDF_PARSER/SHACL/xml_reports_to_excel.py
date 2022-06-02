e55cba3af5ca42cd69a9585388fdeca39b375d0a
kristjan.vilgo <kristjan.vilgo@gmail.com>
2022-06-02 08:33:00 +0300

    Latest changes to SUV xml_reports_to_excel.py synced

--- Tools/RDF_PARSER/SHACL/xml_reports_to_excel.py
+++ Tools/RDF_PARSER/SHACL/xml_reports_to_excel.py
@@ -19,34 +19,87 @@
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
+#
+# Change history
+#Date	    By	Description
+#2021-05-14 LOO Change history added
+#2021-05-14 LOO File paths updated with hardcoded locations
+#2021-05-17 LOO Use of arguments generated from library.xml added
+#2021-05-21 LOO Updated version from Kristjan used
+#2021-05-26 KV  Updated parse_children_to_dict to pick up all children and all attributes, add per TSO name excel sheet, with validation results, added additional logging
+#2021-06-02 LOO Four arguments are delivered, arg0 is the path to the script location.
+#2021-06-22 KV  Check if there is sourcingTSO defined in metadata before doing aggregations per TSO; Check if any input data is provided and log invalid paths; improve xml parsing speed
 
-import pandas
+import pandas, sys
 from lxml import etree
 from datetime import datetime
 import os
 
 def parse_children_to_dict(parent):
-    """Parses all parent direct children to dictionary.
-    If attrib is present then that is assigned as value, else the text"""
+    """Parses all children to flat dictionary.
+    If attribs are present then first is assigned as value, else the text"""
 
     result_dict = {}
-    for child in parent.getchildren():
-        child_name = child.tag.split("}")[1]
+
+    children = parent.getchildren()
+
+    for child in children:
+        child_namespace, child_name = child.tag.split("}")
+
+        if len(child.getchildren()) > 0:
+            children.extend(child.getchildren())
 
         if len(child.attrib) > 0:
-            result_dict[child_name] = child.attrib.values()[0]
-        else:
-            result_dict[child_name] = child.text
+
+            for attribute in child.attrib.items():
+                attribute, attribute_value = attribute
+                attribute_namespace, attribute_name = attribute.split("}")
+
+            result_dict[f"{child_name}_{attribute_name}"] = attribute_value
+
+        if child.text and child.text.strip():
+            result_dict[child_name] = child.text.strip()
 
     return result_dict
 
 # Get list of report files
-current_directory = os.getcwd()
-#xml_report_dir = r"temp\preproReports"
-#input_files = os.listdir(os.path.join(current_directory, xml_report_dir))
 
-# Sample how to give single path
-input_files = [r"ReportExample.xml"]
+file_reports = ""
+model_reports = ""
+output = ""
+
+# If we got arguments use them, otherwise assume a folder structure relative current working directory
+# TODO - put output to the first position and any number of input paths as a list
+    
+if len(sys.argv) == 4:
+    file_reports    = sys.argv[1]
+    model_reports   = sys.argv[2]
+    output          = sys.argv[3]
+else:
+    base_path       = os.getcwd()
+    file_reports    = os.path.join(base_path, "workspace/output/temp/preproReports")
+    model_reports   = os.path.join(base_path, "workspace/output/temp/modelReport")
+    output          = os.path.join(base_path, "workspace/output/report")
+
+output_name = os.path.join(output, f'ExcelValidationReport_{datetime.now():%Y%m%d_%H%M%S}.xlsx')
+
+
+# Create the list of input files
+
+input_files = []
+
+for input_path in [file_reports, model_reports]:
+
+    if os.path.exists(input_path):
+        input_files.extend(os.path.join(input_path, file_path) for file_path in os.listdir(input_path))
+
+    else:
+        print(f"[{datetime.now():%Y-%m-%d %H:%M:%S}] [ERROR  ] Path not existing -> {input_path}")
+
+if len(input_files) == 0:
+    print(f"[{datetime.now():%Y-%m-%d %H:%M:%S}] [WARNING] No SHACL reports provided -> exiting script, no report will be generated")
+    exit()
+
 
 # Create dictionary to store all reports
 validation_report_name = "Validations"
@@ -54,12 +107,15 @@ reports = {validation_report_name: []}
 
 
 # Read in all reports
+parser = etree.XMLParser(remove_comments=True, collect_ids=False, remove_blank_text=True)  # Improve parsing speed
 for path in input_files:
 
-    report_xml = etree.parse(os.path.join(current_directory, path))
+    print(f"[{datetime.now():%Y-%m-%d %H:%M:%S}] [INFO   ] Parsing report {path}")
+
+    report_xml = etree.parse(path, parser=parser)
 
     # Extract all metadata
-    models_meta_objects = report_xml.findall(".//{*}FullModel")
+    models_meta_objects = report_xml.findall(".//{*}Report.MetaData/{*}FullModel")
     models_meta = {}
     for model in models_meta_objects:
 
@@ -100,23 +156,18 @@ for path in input_files:
 report_data = pandas.DataFrame(reports[validation_report_name])
 
 # Number of severity
-severity = report_data.resultSeverity.value_counts()
+severity = report_data.resultSeverity_resource.value_counts()
 
 # Number of warning per Rule
-rule_warnings = report_data.query("resultSeverity == '#WARNING'").sourceShape.value_counts()
+rule_warnings = report_data.query("resultSeverity_resource == '#WARNING'").sourceShape_resource.value_counts()
 
 # Number of errors per Rule
-rule_errors = report_data.query("resultSeverity == '#ERROR'").sourceShape.value_counts()
+rule_errors = report_data.query("resultSeverity_resource == '#ERROR'").sourceShape_resource.value_counts()
 
-# Number of errors per TSO
-tso_warnings = report_data.query("resultSeverity == '#WARNING'")["Model.sourcingTSO"].value_counts()
-
-# Number of errors per TSO
-tso_errors = report_data.query("resultSeverity == '#ERROR'")["Model.sourcingTSO"].value_counts()
 
 # Export to excel
-file_name = f'Report_{datetime.now():%Y%m%d_%H%M%S}.xlsx'
-with pandas.ExcelWriter(file_name) as writer:
+print(f"[{datetime.now():%Y-%m-%d %H:%M:%S}] [INFO   ] All reports imported, starting to create Excel report")
+with pandas.ExcelWriter(output_name, options={'strings_to_urls': False}) as writer:
 
     # Raw reports
     for report_name, report in reports.items():
@@ -126,11 +177,23 @@ with pandas.ExcelWriter(file_name) as writer:
     severity.to_excel(writer, sheet_name='Overall Severity')
     rule_warnings.to_excel(writer, sheet_name='Warnings by Rule')
     rule_errors.to_excel(writer, sheet_name='Errors by Rule')
-    tso_warnings.to_excel(writer, sheet_name='Warnings by TSO')
-    tso_errors.to_excel(writer, sheet_name='Errors by TSO')
 
+    # Additional statistics if sourcingTSO is present in metadata
+    if "Model.sourcingTSO" in report_data.columns:
 
-print(f"[{datetime.now():%Y-%m-%d %H:%M:%S}] [INFO   ] Excel report created -> {file_name}")
+        # Number of errors per TSO
+        report_data.query("resultSeverity_resource == '#WARNING'")["Model.sourcingTSO"].value_counts().to_excel(writer, sheet_name='Warnings by TSO')
+
+        # Number of errors per TSO
+        report_data.query("resultSeverity_resource == '#ERROR'")["Model.sourcingTSO"].value_counts().to_excel(writer, sheet_name='Errors by TSO')
+
+        # Report per TSO if more than one TSO present
+        TSOs = report_data["Model.sourcingTSO"].unique()
+        if len(TSOs) > 2:
+            for TSO in TSOs:
+                report_data[report_data["Model.sourcingTSO"] == TSO].to_excel(writer, sheet_name=f"{validation_report_name}_{TSO}")
+
+print(f"[{datetime.now():%Y-%m-%d %H:%M:%S}] [INFO   ] Excel report created -> {output_name}")
 
 
 
